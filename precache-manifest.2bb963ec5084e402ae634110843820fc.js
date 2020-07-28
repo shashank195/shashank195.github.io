077045b2eba548b7a0addd0233f6650e3c786d84
Shashank Kumar <shashank.cs18@bmsce.ac.in>
2020-07-29 00:08:02 +0530

    Updates

---
+++ precache-manifest.2bb963ec5084e402ae634110843820fc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d69608e422260c83bdd544710c24ae3d",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "88d7d44951adbdd01cfc",
+    "url": "/home/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "0426497d1eefb58389da",
+    "url": "/home/static/css/main.5f05969b.chunk.css"
+  },
+  {
+    "revision": "88d7d44951adbdd01cfc",
+    "url": "/home/static/js/2.8fa335fd.chunk.js"
+  },
+  {
+    "revision": "80fa05aac9f80c71b5f99ecec6ead10d",
+    "url": "/home/static/js/2.8fa335fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0426497d1eefb58389da",
+    "url": "/home/static/js/main.862ebd19.chunk.js"
+  },
+  {
+    "revision": "5c07ecca0456e28e04f0",
+    "url": "/home/static/js/runtime-main.e084aacb.js"
+  },
+  {
+    "revision": "d48591c3ff900fda2e52e6086dc8ace0",
+    "url": "/home/static/media/resume.d48591c3.pdf"
+  }
+]);
