393fc46d374ba7df7122732136278235ec4dd343
Shashank <shashank.cs18@bmsce.ac.in>
2020-08-17 00:15:53 +0530

    Updates

---
+++ precache-manifest.23608c16adddd510b3c2d8be9e4e7352.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57b6c77ead7b8838ef313106ec5ded05",
+    "url": "/index.html"
+  },
+  {
+    "revision": "874b47e21b1d5c43200c",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "fa740b1e9b1035624269",
+    "url": "/static/css/main.724e3e34.chunk.css"
+  },
+  {
+    "revision": "874b47e21b1d5c43200c",
+    "url": "/static/js/2.6f50c311.chunk.js"
+  },
+  {
+    "revision": "80fa05aac9f80c71b5f99ecec6ead10d",
+    "url": "/static/js/2.6f50c311.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa740b1e9b1035624269",
+    "url": "/static/js/main.5493256b.chunk.js"
+  },
+  {
+    "revision": "c88671eb88bf6c28dee9",
+    "url": "/static/js/runtime-main.6a060e8a.js"
+  },
+  {
+    "revision": "91fa76825fcd5bea7615ae237c7cb055",
+    "url": "/static/media/resume.91fa7682.pdf"
+  },
+  {
+    "revision": "60a2302f307dd75771f3305d7547bdf4",
+    "url": "/static/media/shashank.60a2302f.jpg"
+  }
+]);
